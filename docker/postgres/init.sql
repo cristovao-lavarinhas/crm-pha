d96d25c0119c110f0f6c10bb87c0e16d4afffc9c
Cristóvão Lavarinhas <cristovao.lavarinhas@ipvc.pt>
2025-07-22 01:46:55 +0100

    update

--- docker/postgres/init.sql
+++
@@ -1,9 +0,0 @@
--- Criação do banco de dados e usuário
-CREATE DATABASE crm_pharma;
-CREATE USER pharma_user WITH ENCRYPTED PASSWORD 'pharma_password';
-GRANT ALL PRIVILEGES ON DATABASE crm_pharma TO pharma_user;
-
--- Extensões úteis para PostgreSQL
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE EXTENSION IF NOT EXISTS "pg_trgm";
-CREATE EXTENSION IF NOT EXISTS "unaccent";
